85b950b8759c136b34d9e6bc17bddf52b220ec71
Muhammed Humraz <muhammadhamraz201@gmail.com>
2025-11-09 22:21:44 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,11 @@
+def check_even(n)
+ if n % 2 == 0:
+   return true
+ else:
+   return false
+ n = int(input("enter the number "))  
+ check_even(n)
+  if check_even(n):
+    print("the number is even")
+  else:
+     print("the number is odd")
